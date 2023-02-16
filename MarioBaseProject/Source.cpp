8b80f43d986b3cc78aa11165d797d58a3a987350
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2023-02-16 19:23:19 +0000

    Initial Commit

---
+++ MarioBaseProject/Source.cpp
@@ -0,0 +1,9 @@
+//Be grateful for humble beginnings, because the next level will always require so much more of you
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <SDL_image.h>
+
+int main(int argc, char* args[])
+{
+	return 0;
+}
