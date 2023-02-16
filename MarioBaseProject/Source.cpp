31206be67886dfa192d27022699254e4a3eee3ca
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2023-02-16 19:41:17 +0000

    Completed Week 1

--- MarioBaseProject/Source.cpp
+++ MarioBaseProject/Source.cpp
@@ -1,9 +1,69 @@
-//Be grateful for humble beginnings, because the next level will always require so much more of you
 #include <SDL.h>
 #include <SDL_mixer.h>
 #include <SDL_image.h>
+#include "Constants.h"
+#include <iostream>
+using namespace std;
+
+//GlobalVariables
+SDL_Window* g_window = nullptr;
+
+//FunctionPrototypes
+bool InitSDL();
+void CloseSDL();
+
 
 int main(int argc, char* args[])
 {
+	//check if sdl was setup correctly
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+
+	CloseSDL();
 	return 0;
-}b 
+
+}
+
+
+
+
+bool InitSDL()
+{
+	//Setup SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		//setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		//did the window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+	}
+}
+
+void CloseSDL()
+{
+	//release the window
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+
+	//quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+
+}
