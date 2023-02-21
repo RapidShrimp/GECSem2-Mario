7d0f10c21fa898edc1da20ed20e3a5da2154c70b
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2023-02-21 10:12:17 +0000

    Rendering
    Image of Darth Vader Now Displays

--- MarioBaseProject/Source.cpp
+++ MarioBaseProject/Source.cpp
@@ -7,11 +7,16 @@ using namespace std;
 
 //GlobalVariables
 SDL_Window* g_window = nullptr;
+SDL_Renderer* g_renderer = nullptr;
+SDL_Texture* g_texture = nullptr;
 
 //FunctionPrototypes
 bool InitSDL();
 void CloseSDL();
 bool Update();
+void Render();
+SDL_Texture* LoadTextureFromFile(string path);
+void FreeTexture();
 
 
 int main(int argc, char* args[])
@@ -22,6 +27,7 @@ int main(int argc, char* args[])
 		bool quit = false;
 		while (!quit)
 		{
+			Render();
 			quit = Update();
 		}
 	}
@@ -48,6 +54,7 @@ bool InitSDL()
 			SCREEN_WIDTH,
 			SCREEN_HEIGHT,
 			SDL_WINDOW_SHOWN);
+
 		//did the window get created?
 		if (g_window == nullptr)
 		{
@@ -55,7 +62,32 @@ bool InitSDL()
 			cout << "Window was not created. Error: " << SDL_GetError();
 			return false;
 		}
+
+		//Attach Renderer To Window
+		g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+		if (g_renderer != nullptr) 
+		{
+			int imageFlags = IMG_INIT_PNG;
+			if (!(IMG_Init(imageFlags) & imageFlags)) 
+			{
+				cout << "SDL_Image could not initialise. Error: " << IMG_GetError() << endl;
+				return false;
+			}
+		}
+		else 
+		{
+			cout << "SDL_Renderer Could not initialise. Error: " << SDL_GetError() << endl;
+			return false;
+		}
 	}
+	g_texture = LoadTextureFromFile("Images/test.bmp");
+	if (g_texture == nullptr) 
+	{
+		cout << "Couldnt Load Image" << SDL_GetError()<< endl;
+		return false;
+	}
+
+	return true;
 }
 
 void CloseSDL()
@@ -63,6 +95,10 @@ void CloseSDL()
 	//release the window
 	SDL_DestroyWindow(g_window);
 	g_window = nullptr;
+
+	FreeTexture();
+	SDL_DestroyRenderer(g_renderer);
+	g_renderer = nullptr;
 
 	//quit SDL subsystems
 	IMG_Quit();
@@ -86,4 +122,49 @@ bool Update()
 		break;
 	}
 	return false;
+}
+
+void Render()
+{
+	//Clear Screen
+	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(g_renderer);
+	//Set Render Location
+	SDL_Rect renderLocation = { 0,0,SCREEN_WIDTH,SCREEN_HEIGHT };
+	//Render To Screen
+	SDL_RenderCopyEx(g_renderer, g_texture, NULL, &renderLocation, 0, NULL, SDL_FLIP_NONE);
+	//Update Screen
+	SDL_RenderPresent(g_renderer);
+}
+
+SDL_Texture* LoadTextureFromFile(string path)
+{
+	//Remove Memory of Previous Texture
+	FreeTexture();
+	SDL_Texture* p_Texture = nullptr;
+
+	//Load Image
+	SDL_Surface* p_Surface = IMG_Load(path.c_str());
+	if (p_Surface != nullptr) 
+	{
+		p_Texture = SDL_CreateTextureFromSurface(g_renderer, p_Surface);
+		if (p_Texture == nullptr) 
+		{
+			cout << "Unable to Create Texture From Surface. Error:" << SDL_GetError() << endl;
+		}
+		SDL_FreeSurface(p_Surface);
+	}
+	else
+	{
+		cout << "Unable to create texture from surface. Error:" << IMG_GetError() << endl;
+	}
+	return p_Texture;
+}
+
+void FreeTexture()
+{
+	if (g_texture != nullptr) {
+		SDL_DestroyTexture(g_texture);
+		g_texture = nullptr;
+	}
 }
