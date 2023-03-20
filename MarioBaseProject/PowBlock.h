98917220a454809c5cb35ba30abdc053d1dd2c3d
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2023-03-20 23:33:18 +0000

    Pow CTD

---
+++ MarioBaseProject/PowBlock.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef _POWBLOCK_H
+#define _POWBLOCK_H
+#include "Commons.h"
+
+class Texture2D;
+class LevelMap;
+class Vector2D;
+class SDL_Renderer;
+
+class PowBlock
+{
+public:
+	PowBlock(SDL_Renderer* renderer, LevelMap* map);
+	~PowBlock();
+	void Render();
+	void TakeHit();
+	inline Rect2D GetCollisionBox();
+	inline bool IsAvailable() { return m_num_hits_left > 0; }
+
+private:
+	SDL_Renderer* m_renderer;
+	Vector2D m_position;
+	Texture2D* m_texture;
+	LevelMap* m_level_map;
+
+	float m_single_sprite_w;
+	float m_single_sprite_h;
+	int m_num_hits_left;
+
+};
+
+#endif // !_POWBLOCK_H
