27c6a2f6b186f801fe2e74cbb8cd6c3ef2e446af
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2023-05-05 17:07:25 +0100

    Main Menu Text

--- MarioBaseProject/ScreenMainMenu.cpp
+++ MarioBaseProject/ScreenMainMenu.cpp
@@ -20,9 +20,29 @@ ScreenMainMenu::~ScreenMainMenu()
 
 void ScreenMainMenu::Render()
 {
+	//Title
 	SDL_Rect MenuPortion = { 0,0,240,96 };
-	SDL_Rect Position = {0,0,SCREEN_WIDTH,SCREEN_HEIGHT/2};
-	m_background_texture->Render(MenuPortion,Position, SDL_FLIP_NONE);
+	SDL_Rect MenuPosition = { 0,0,SCREEN_WIDTH,SCREEN_HEIGHT / 2 };
+	m_background_texture->Render(MenuPortion, MenuPosition, SDL_FLIP_NONE);
+	
+
+	//Text
+	 m_SingleplayerText->Render(125, SCREEN_HEIGHT/2 + 50);
+	 m_MultiplayerText->Render(125, SCREEN_HEIGHT / 2 + 110);
+	//Character Selector
+
+	SDL_Rect SelectPortion = { 52,106,9,9 };
+	SDL_Rect SelectPosition;
+	if (IsPlayer1)
+	{
+		SelectPosition = { 80,SCREEN_HEIGHT / 2 + 58, 30,30};
+	}
+	else 
+	{ 
+		SelectPosition = { 80,SCREEN_HEIGHT / 2 + 118, 30,30 };
+	}
+	m_character_select->Render(SelectPortion, SelectPosition, SDL_FLIP_NONE);
+
 }
 
 void ScreenMainMenu::Update(float deltaTime, SDL_Event e)
@@ -33,7 +53,7 @@ void ScreenMainMenu::Update(float deltaTime, SDL_Event e)
 		switch (e.key.keysym.sym)
 		{
 		case SDLK_SPACE:
-			m_screen_manager->ChangeScreen(SCREEN_LEVEL2);
+			m_screen_manager->ChangeScreen(SCREEN_LEVEL1);
 			break;
 		}
 	}
@@ -45,6 +65,24 @@ bool ScreenMainMenu::SetupMenu()
 	if (!m_background_texture->LoadFromFile("Images/SpriteSheet.png"))
 	{
 		cout << "Failed to Load Background Texture" << endl;
+		return false;
+	}
+	m_character_select = new Texture2D(m_renderer);
+	if (!m_character_select->LoadFromFile("Images/SpriteSheet.png")) 
+	{
+		cout << "Failed to Load Character Selector" << endl;
+		return false;
+	}
+	m_SingleplayerText = new TextRenderer(m_renderer);
+	if (!m_SingleplayerText->LoadFont("Fonts/PixelFont.ttf", 34, "Singleplayer", { 255, 130, 10, 255 }))
+	{
+		cout << "Failed to Load Singleplayer Text Font" << endl;
+		return false;
+	}
+	m_MultiplayerText = new TextRenderer(m_renderer);
+	if (!m_MultiplayerText->LoadFont("Fonts/PixelFont.ttf", 34, "Multiplayer", { 255, 130, 10, 255 }))
+	{
+		cout << "Failed to Load Mutliplayer Text Font" << endl;
 		return false;
 	}
 	return true;
--- MarioBaseProject/ScreenMainMenu.h
+++ MarioBaseProject/ScreenMainMenu.h
@@ -2,7 +2,11 @@
 #include "GameScreen.h"
 #include "GameScreenManager.h"
 #include "Texture2D.h"
+#include "TextRenderer.h"
+
 class Texture2D;
+class TextRenderer;
+
 
 class ScreenMainMenu : GameScreen
 {
@@ -10,10 +14,15 @@ public :
 	ScreenMainMenu(SDL_Renderer* renderer, GameScreenManager* manager);
 	~ScreenMainMenu();
 	void Render();
+	bool IsPlayer1;
+	void SelectOther() { IsPlayer1 == !IsPlayer1; }
 private:
 	void Update(float deltaTime, SDL_Event e);
 	bool SetupMenu();
 	Texture2D* m_background_texture;
+	Texture2D* m_character_select;
+	TextRenderer* m_SingleplayerText;
+	TextRenderer* m_MultiplayerText;
 	Vector2D m_background_position;
 	SDL_Renderer* m_renderer;
 	GameScreenManager* m_screen_manager;
